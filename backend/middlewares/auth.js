8bd6ae6377d1e6760c0f03cb367b5d38156e3b56
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-11-01 16:23:50 +0300

    add frontend support

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const AuthorizationError = require('../errors/AuthorizationError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
@@ -11,7 +13,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new AuthorizationError('Ошибка авторизации'));
   }
