7a009c444d33f42a6676ad550799c28a8bf3d557
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-11-11 17:33:56 +0300

    clear build

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -11,9 +11,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new AuthorizationError('Ошибка авторизации'));
   }
 
   const token = authorization.replace('Bearer ', '');
