cae6f4367b99511e2be99cb8b8abb2c80574d9ef
Timophey Shalonsky <shalonsky.tim@yandex.ru>
2022-11-12 07:13:58 +0300

    fix like-func

--- backend/test.js
+++ backend/test.js
@@ -4,6 +4,7 @@ const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzYyM2UxMDg1M
 const SECRET_KEY_DEV = 'dev-secret'; // вставьте сюда секретный ключ для разработки из кода
 try {
   const payload = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
+  console.log(payload);
   console.log('\x1b[31m%s\x1b[0m', `
 Надо исправить. В продакшне используется тот же
 секретный ключ, что и в режиме разработки.
