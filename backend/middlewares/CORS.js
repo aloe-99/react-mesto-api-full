ab74c703132af2753d00daec8f326af5b5688f60
aloe-99 <100123642+aloe-99@users.noreply.github.com>
2023-05-09 03:49:45 +0300

    clear build

--- backend/middlewares/CORS.js
+++ backend/middlewares/CORS.js
@@ -19,5 +19,5 @@ module.exports = (req, res, next) => {
     return res.end();
   }
 
-  next();
+  return next();
 };
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'karambola');
   } catch (err) {
     next(new AuthorizationError('Ошибка авторизации'));
   }
