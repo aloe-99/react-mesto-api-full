90f19783bc90cf91d0653ecf9926f377f763fe32
aloe-99 <100123642+aloe-99@users.noreply.github.com>
2023-05-10 15:11:56 +0300

    update README

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const CORS = require('./middlewares/CORS');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
